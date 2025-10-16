c359ac45efabc45dc6c92c32ff8fdd591f2101fe
Kasper Engelen <kasper.engelen@hotmail.com>
2025-10-16 20:08:15 +0200

    The commands now have long and short versions. The "verbose" command can now be specified multiple times.

--- src/arg_parser.hh
+++ src/arg_parser.hh
@@ -11,7 +11,8 @@ namespace po = boost::program_options;
  * Struct that will hold the parsed argument values.
  */
 struct ArgParseResult {
-
+  // TODO: add arguments here
+  // int num_verbose;
 };
 
 /**
@@ -22,7 +23,7 @@ struct ArgParseResult {
  *
  * @return ArgParseResult object with the argument values.
  */
-ArgParseResult arg_parser(int argc, char **argv) {
+inline ArgParseResult arg_parser(int argc, char **argv) {
   debug_("Parsing arguments.");
   try {
     po::options_description desc("Allowed options");
@@ -31,9 +32,9 @@ ArgParseResult arg_parser(int argc, char **argv) {
     ("version", "print program version")
 
     // ??
-    ("f,formula", po::value<std::string>()->value_name("STRING"),
+    ("formula,f", po::value<std::string>()->value_name("STRING"),
       "process the formula STRING")
-    ("F,file", po::value<std::string>()->value_name("FILENAME[/COL]"),
+    ("file,F", po::value<std::string>()->value_name("FILENAME[/COL]"),
       "process each line of FILENAME as a formula; if COL "
                                                 "is a positive integer, assume a CSV file and read "
                                                 "column COL; use a negative COL to drop the first "
@@ -45,28 +46,28 @@ ArgParseResult arg_parser(int argc, char **argv) {
                              "subformulas are considered as atomic properties")
 
     // input options
-    ("0,init", po::value<std::string>()->value_name("STATE"),
+    ("init,0", po::value<std::string>()->value_name("STATE"),
       "comma-separated state vector to use as initial state")
-    ("i,ins", po::value<std::string>()->value_name("PROPS"),
+    ("ins,i", po::value<std::string>()->value_name("PROPS"),
       "comma-separated list of uncontrollable (a.k.a. input) atomic propositions")
-    ("j,workers", po::value<std::string>()->value_name("VAL"),
+    ("workers,j", po::value<std::string>()->value_name("VAL"),
       "Number of parallel workers for composition")
-    ("o,outs", po::value<std::string>()->value_name("PROPS"),
+    ("outs,o", po::value<std::string>()->value_name("PROPS"),
       "comma-separated list of controllable (a.k.a. output) atomic propositions")
-    ("S,synth", po::value<std::string>()->value_name("FILENAME"),
+    ("synth,S", po::value<std::string>()->value_name("FILENAME"),
       "enable synthesis, pass .aag filename, or - to print gates")
-    ("W,winreg", po::value<std::string>()->value_name("FILENAME"),
+    ("winreg,W", po::value<std::string>()->value_name("FILENAME"),
       "output winning region, pass .aag filename, or - to print gates")
 
     // fine tuning
-    ("I,Kinc", po::value<std::string>()->value_name("VAL"),
+    ("Kinc,I", po::value<std::string>()->value_name("VAL"),
       "increment value for K, used when Kmin < K")
     ("K", po::value<std::string>()->value_name("VAL"),
       "final value of K, or unique value if Kmin is not specified")
-    ("M,Kmin", po::value<std::string>()->value_name("VAL"),
+    ("Kmin,M", po::value<std::string>()->value_name("VAL"),
       "starting value of K; Kinc MUST be set when using "
                              "this option")
-    ("u,unreql-x", po::value<std::string>()->value_name("[formula|automaton|both]"),
+    ("unreal-x,u", po::value<std::string>()->value_name("[formula|automaton|both]"),
       "for unrealizability, either add X's to outputs in "
                              "the input formula, or push outputs one transition "
                              "forward in the automaton; with 'both', two "
@@ -74,11 +75,15 @@ ArgParseResult arg_parser(int argc, char **argv) {
                              "(default: both).")
 
     // output options
-    ("c,check", po::value<std::string>()->value_name("[real|unreal|both]"),
+    ("check,c", po::value<std::string>()->value_name("[real|unreal|both]"),
       "either check for real, unreal, or both")
-    ("v,verbose", po::bool_switch()->default_value(false)->zero_tokens(),
+    // NOTE: this weird construction such that "verbose" can be specified multiple times
+    ("verbose,v", po::value<std::vector<bool>>()
+      ->default_value(std::vector<bool>(), "false")
+      ->implicit_value(std::vector<bool>(1), "true")
+      ->zero_tokens(),
       "verbose mode, can be repeated for more verbosity")
-    ;
+      ;
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -91,10 +96,12 @@ ArgParseResult arg_parser(int argc, char **argv) {
 
     // TODO: extract information here
 
+    debug_("Finished parsing arguments.");
+
     return ArgParseResult();
 
   } catch (const std::exception& ex) {
-
+    std::cout << ex.what() << std::endl;
     exit(3);
   }
 
