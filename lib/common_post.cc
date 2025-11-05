fac681c47d7f8f2d07eb84cfb4b3488f8ca00d8e
Kasper Engelen <kasper.engelen@hotmail.com>
2025-11-05 13:38:23 +0100

    Slowly removing common stuff

--- lib/common_post.cc
+++ lib/common_post.cc
@@ -25,11 +25,11 @@
 #include "argmatch.h"
 
 spot::postprocessor::output_type type = spot::postprocessor::TGBA;
-spot::postprocessor::output_pref pref = spot::postprocessor::Small;
-spot::postprocessor::output_pref comp = spot::postprocessor::Any;
-spot::postprocessor::output_pref sbacc = spot::postprocessor::Any;
-spot::postprocessor::output_pref colored = spot::postprocessor::Any;
-spot::postprocessor::optimization_level level = spot::postprocessor::High;
+// spot::postprocessor::output_pref pref = spot::postprocessor::Small;
+// spot::postprocessor::output_pref comp = spot::postprocessor::Any;
+// spot::postprocessor::output_pref sbacc = spot::postprocessor::Any;
+// spot::postprocessor::output_pref colored = spot::postprocessor::Any;
+// spot::postprocessor::optimization_level level = spot::postprocessor::High;
 
 bool level_set = false;
 bool pref_set = false;
@@ -152,119 +152,119 @@ static const argp_option options_disabled[] =
     { nullptr, 0, nullptr, 0, nullptr, 0 }
   };
 
-static int
-parse_opt_post(int key, char* arg, struct argp_state*)
-{
-  // Called from C code, so should not raise any exception.
-  BEGIN_EXCEPTION_PROTECT;
-  // This switch is alphabetically-ordered.
-  switch (key)
-    {
-    case 'a':
-      pref = spot::postprocessor::Any;
-      pref_set = true;
-      break;
-    case 'B':
-      type = spot::postprocessor::BA;
-      colored = spot::postprocessor::Any;
-      sbacc = spot::postprocessor::SBAcc;
-      break;
-    case 'C':
-      comp = spot::postprocessor::Complete;
-      break;
-    case 'D':
-      pref = spot::postprocessor::Deterministic;
-      pref_set = true;
-      break;
-    case 'G':
-      type = spot::postprocessor::Generic;
-      colored = spot::postprocessor::Any;
-      break;
-    case 'M':
-      type = spot::postprocessor::Monitor;
-      colored = spot::postprocessor::Any;
-      break;
-    case 'P':
-    case 'p':
-      {
-        static char const *const parity_args[] =
-          {
-            "any", "min", "max", "odd", "even",
-            "min odd", "odd min",
-            "min even", "even min",
-            "max odd", "odd max",
-            "max even", "even max",
-            nullptr
-          };
-        static spot::postprocessor::output_type const parity_types[] =
-          {
-            spot::postprocessor::Parity,
-            spot::postprocessor::ParityMin,
-            spot::postprocessor::ParityMax,
-            spot::postprocessor::ParityOdd,
-            spot::postprocessor::ParityEven,
-            spot::postprocessor::ParityMinOdd,
-            spot::postprocessor::ParityMinOdd,
-            spot::postprocessor::ParityMinEven,
-            spot::postprocessor::ParityMinEven,
-            spot::postprocessor::ParityMaxOdd,
-            spot::postprocessor::ParityMaxOdd,
-            spot::postprocessor::ParityMaxEven,
-            spot::postprocessor::ParityMaxEven,
-          };
-        ARGMATCH_VERIFY(parity_args, parity_types);
-        if (arg)
-          type = XARGMATCH(key == 'P' ? "--parity" : "--colored-parity",
-                           arg, parity_args, parity_types);
-        else
-          type = spot::postprocessor::Parity;
-        if (key == 'p')
-          colored = spot::postprocessor::Colored;
-      }
-      break;
-    case 'S':
-      sbacc = spot::postprocessor::SBAcc;
-      break;
-    case OPT_COBUCHI:
-      type = spot::postprocessor::CoBuchi;
-      break;
-    case OPT_HIGH:
-      level = spot::postprocessor::High;
-      simplification_level = 3;
-      level_set = true;
-      break;
-    case OPT_LOW:
-      level = spot::postprocessor::Low;
-      simplification_level = 1;
-      level_set = true;
-      break;
-    case OPT_MEDIUM:
-      level = spot::postprocessor::Medium;
-      simplification_level = 2;
-      level_set = true;
-      break;
-    case OPT_SMALL:
-      pref = spot::postprocessor::Small;
-      pref_set = true;
-      break;
-    case OPT_TGBA:
-      if (automaton_format == Spin)
-        error(2, 0, "--spin and --tgba are incompatible");
-      type = spot::postprocessor::TGBA;
-      colored = spot::postprocessor::Any;
-      break;
-    default:
-      return ARGP_ERR_UNKNOWN;
-    }
-  END_EXCEPTION_PROTECT;
-  return 0;
-}
-
-const struct argp post_argp = { options, parse_opt_post,
-                                nullptr, nullptr, nullptr, nullptr, nullptr };
-const struct argp post_argp_disabled = { options_disabled, parse_opt_post,
-                                         nullptr, nullptr, nullptr,
-                                         nullptr, nullptr };
-const struct argp post_argp_nooutput = { options_nooutput, parse_opt_post,
-                                         nullptr, nullptr, nullptr,
-                                         nullptr, nullptr };
+// static int
+// parse_opt_post(int key, char* arg, struct argp_state*)
+// {
+//   // Called from C code, so should not raise any exception.
+//   BEGIN_EXCEPTION_PROTECT;
+//   // This switch is alphabetically-ordered.
+//   switch (key)
+//     {
+//     case 'a':
+//       pref = spot::postprocessor::Any;
+//       pref_set = true;
+//       break;
+//     case 'B':
+//       type = spot::postprocessor::BA;
+//       colored = spot::postprocessor::Any;
+//       sbacc = spot::postprocessor::SBAcc;
+//       break;
+//     case 'C':
+//       comp = spot::postprocessor::Complete;
+//       break;
+//     case 'D':
+//       pref = spot::postprocessor::Deterministic;
+//       pref_set = true;
+//       break;
+//     case 'G':
+//       type = spot::postprocessor::Generic;
+//       colored = spot::postprocessor::Any;
+//       break;
+//     case 'M':
+//       type = spot::postprocessor::Monitor;
+//       colored = spot::postprocessor::Any;
+//       break;
+//     case 'P':
+//     case 'p':
+//       {
+//         static char const *const parity_args[] =
+//           {
+//             "any", "min", "max", "odd", "even",
+//             "min odd", "odd min",
+//             "min even", "even min",
+//             "max odd", "odd max",
+//             "max even", "even max",
+//             nullptr
+//           };
+//         static spot::postprocessor::output_type const parity_types[] =
+//           {
+//             spot::postprocessor::Parity,
+//             spot::postprocessor::ParityMin,
+//             spot::postprocessor::ParityMax,
+//             spot::postprocessor::ParityOdd,
+//             spot::postprocessor::ParityEven,
+//             spot::postprocessor::ParityMinOdd,
+//             spot::postprocessor::ParityMinOdd,
+//             spot::postprocessor::ParityMinEven,
+//             spot::postprocessor::ParityMinEven,
+//             spot::postprocessor::ParityMaxOdd,
+//             spot::postprocessor::ParityMaxOdd,
+//             spot::postprocessor::ParityMaxEven,
+//             spot::postprocessor::ParityMaxEven,
+//           };
+//         ARGMATCH_VERIFY(parity_args, parity_types);
+//         if (arg)
+//           type = XARGMATCH(key == 'P' ? "--parity" : "--colored-parity",
+//                            arg, parity_args, parity_types);
+//         else
+//           type = spot::postprocessor::Parity;
+//         if (key == 'p')
+//           colored = spot::postprocessor::Colored;
+//       }
+//       break;
+//     case 'S':
+//       sbacc = spot::postprocessor::SBAcc;
+//       break;
+//     case OPT_COBUCHI:
+//       type = spot::postprocessor::CoBuchi;
+//       break;
+//     case OPT_HIGH:
+//       level = spot::postprocessor::High;
+//       simplification_level = 3;
+//       level_set = true;
+//       break;
+//     case OPT_LOW:
+//       level = spot::postprocessor::Low;
+//       simplification_level = 1;
+//       level_set = true;
+//       break;
+//     case OPT_MEDIUM:
+//       level = spot::postprocessor::Medium;
+//       simplification_level = 2;
+//       level_set = true;
+//       break;
+//     case OPT_SMALL:
+//       pref = spot::postprocessor::Small;
+//       pref_set = true;
+//       break;
+//     case OPT_TGBA:
+//       if (automaton_format == Spin)
+//         error(2, 0, "--spin and --tgba are incompatible");
+//       type = spot::postprocessor::TGBA;
+//       colored = spot::postprocessor::Any;
+//       break;
+//     default:
+//       return ARGP_ERR_UNKNOWN;
+//     }
+//   END_EXCEPTION_PROTECT;
+//   return 0;
+// }
+//
+// const struct argp post_argp = { options, parse_opt_post,
+//                                 nullptr, nullptr, nullptr, nullptr, nullptr };
+// const struct argp post_argp_disabled = { options_disabled, parse_opt_post,
+//                                          nullptr, nullptr, nullptr,
+//                                          nullptr, nullptr };
+// const struct argp post_argp_nooutput = { options_nooutput, parse_opt_post,
+//                                          nullptr, nullptr, nullptr,
+//                                          nullptr, nullptr };
